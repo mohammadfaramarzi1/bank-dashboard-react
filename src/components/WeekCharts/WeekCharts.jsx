f3add388bd0e6edd8a748b519028b411d4df2db6
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-02-17 09:33:17 +0330

    refactor: dynamic User info

--- src/components/WeekCharts/WeekCharts.jsx
+++ src/components/WeekCharts/WeekCharts.jsx
@@ -1,11 +1,42 @@
 import React, { useEffect, useRef } from "react";
 import { Box, Typography } from "@mui/material";
 import * as echarts from "echarts";
-import { series, option } from "../../Charts/Week";
 
 function WeekCharts() {
   const firstChartRef = useRef(null);
   const secondChartRef = useRef(null);
+  const series = [
+    {
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: "bar",
+      stack: "a",
+      name: "a",
+    },
+    {
+      data: [10, 46, 64, "-", 0, "-", 0],
+      type: "bar",
+      stack: "a",
+      name: "b",
+    },
+    {
+      data: [30, "-", 0, 20, 10, "-", 0],
+      type: "bar",
+      stack: "a",
+      name: "c",
+    },
+    {
+      data: [30, "-", 0, 20, 10, "-", 0],
+      type: "bar",
+      stack: "b",
+      name: "d",
+    },
+    {
+      data: [10, 20, 150, 0, "-", 50, 10],
+      type: "bar",
+      stack: "b",
+      name: "e",
+    },
+  ];
   useEffect(() => {
     if (!firstChartRef.current) return;
     const myChart = echarts.init(firstChartRef.current);
--- src/pages/Setting/Setting.jsx
+++ src/pages/Setting/Setting.jsx
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, Box, Button, IconButton, TextField } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
-import { useDispatch, useSelector } from "react-redux";
-import { getUserInfosFromServer } from "../../Redux/Store/user";
+import { useSelector } from "react-redux";
+import { getUser, setUser } from "../../utils/user";
+import { toast } from "sonner";
 
 function Setting() {
   const user = useSelector((state) => state.user[0]);
   const [form, setForm] = useState({
-    name: "asas",
+    name: "",
     username: "",
     email: "",
     password: "",
   });
+
+  useEffect(() => {
+    setForm(() => getUser());
+  }, []);
+
+  console.log(form);
 
   const changeHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value.trim();
     setForm((form) => ({ ...form, [name]: value }));
   };
+
+  const clickHandler = () => {
+    setUser(form);
+    toast.success("Updated Successfully")
+  }
 
   return (
     <Box
@@ -45,45 +64,82 @@ function Setting() {
           />
         </IconButton>
       </Box>
-      <Box component="form" height="100%">
-        <Box component="div">
-          <TextField
-            name="name"
-            defaultValue={user?.name}
-            onChange={(event) => changeHandler(event)}
-            label="Name"
-            variant="outlined"
-            sx={{ backgroundColor: "#fff" }}
-          />
-          <TextField
-            name="username"
-            defaultValue={user?.username}
-            onChange={(event) => changeHandler(event)}
-            label="Username"
-            variant="outlined"
-            sx={{ ml: 5, backgroundColor: "#fff" }}
-          />
+      <Box
+        component="form"
+        width={700}
+        height={400}
+        sx={{
+          border: 1,
+          borderRadius: 15,
+          px: 3,
+          py: 5,
+          borderColor: "#343C6A",
+        }}
+      >
+        <Typography
+          variant="span"
+          component="p"
+          sx={{
+            fontFamily: "Roboto SemiBold",
+            fontSize: 30,
+            textAlign: "center",
+          }}
+        >
+          USer Info
+        </Typography>
+        <Box
+          component="div"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            rowGap: 3,
+            alignItems: "center",
+            mt: 5,
+            justifyContent: "center",
+            mx: "auto",
+          }}
+        >
+          <Box component="div" sx={{ display: "flex", columnGap: 2 }}>
+            <TextField
+              name="name"
+              value={form.name}
+              onChange={(event) => changeHandler(event)}
+              label="Name"
+              variant="outlined"
+              sx={{ backgroundColor: "#fff", width: 300 }}
+            />
+            <TextField
+              name="username"
+              value={form.username}
+              onChange={(event) => changeHandler(event)}
+              label="Username"
+              variant="outlined"
+              sx={{ ml: 5, backgroundColor: "#fff", width: 300 }}
+            />
+          </Box>
+          <Box component="div" sx={{ display: "flex", columnGap: 2 }}>
+            <TextField
+              name="email"
+              value={form.email}
+              onChange={(event) => changeHandler(event)}
+              label="Email"
+              variant="outlined"
+              sx={{ backgroundColor: "#fff", width: 300 }}
+            />
+            <TextField
+              name="password"
+              value={form.password}
+              onChange={(event) => changeHandler(event)}
+              label="Password"
+              type="password"
+              variant="outlined"
+              sx={{ ml: 5, backgroundColor: "#fff", width: 300 }}
+            />
+          </Box>
+          <Button variant="contained" onClick={clickHandler}>
+            Change
+          </Button>
         </Box>
-        <Box component="div" my={5}>
-          <TextField
-            name="email"
-            defaultValue={user?.email}
-            onChange={(event) => changeHandler(event)}
-            label="Email"
-            variant="outlined"
-            sx={{ backgroundColor: "#fff" }}
-          />
-          <TextField
-            name="password"
-            defaultValue={user?.password}
-            onChange={(event) => changeHandler(event)}
-            label="Password"
-            type="password"
-            variant="outlined"
-            sx={{ ml: 5, backgroundColor: "#fff" }}
-          />
-        </Box>
-        <Button variant="contained">Save</Button>
       </Box>
     </Box>
   );
